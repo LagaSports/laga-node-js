726247d99b3eaea94f51aa2451bc384afd517052
sangsangga <wandaputrasangga@gmail.com>
2024-12-29 19:29:28 +0700

    add env

--- prisma/migrations/20241225161117_create_table_users/migration.sql
+++ prisma/migrations/20241225161117_create_table_users/migration.sql
@@ -1,25 +1,25 @@
 -- CreateTable
 CREATE TABLE IF NOT EXISTS "users" (
     "id" SERIAL NOT NULL,
-    "phoneNumber" VARCHAR,
+    "phone_number" VARCHAR,
     "password" VARCHAR NOT NULL,
     "name" VARCHAR,
     "email" VARCHAR,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "deletedAt" TIMESTAMP(3),
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "deleted_at" TIMESTAMP(3),
 
     CONSTRAINT "users_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "users_phoneNumber_key" ON "users"("phoneNumber");
+CREATE UNIQUE INDEX "users_phone_number_key" ON "users"("phone_number");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
 
 -- CreateIndex
-CREATE INDEX IF NOT EXISTS "users_phoneNumber_idx" ON "users"("phoneNumber");
+CREATE INDEX IF NOT EXISTS "users_phone_number_idx" ON "users"("phone_number");
 
 -- CreateIndex
 CREATE INDEX IF NOT EXISTS "users_email_idx" ON "users"("email");
