66d15555d73ab1d0bb1b1e9b7014a64f93821e9e
sangsangga <wandaputrasangga@gmail.com>
2024-12-29 16:53:20 +0700

    add env

--- src/controller/version-controller.ts
+++ src/controller/version-controller.ts
@@ -3,7 +3,7 @@ import { Request, Response, NextFunction } from "express";
 const getVersion = async (req: Request, res: Response, next: NextFunction) => {
     try {
         // Heroku automatically sets these environment variables
-        const commitHash = process.env.SOURCE_VERSION || 'unknown';
+        const commitHash = process.env.GIT_COMMIT_HASH || 'unknown';
 
         res.status(200).json({
             data: {
